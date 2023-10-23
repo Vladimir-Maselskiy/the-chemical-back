ff423ee02070586a88d2a74de5e03972f3915692
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-10-24 02:16:12 +0300

    fix: word staus >> status

--- controllers/user/getUser.js
+++ controllers/user/getUser.js
@@ -3,7 +3,7 @@ const { User } = require('../../models/user');
 const getUser = async (req, res, next) => {
   const user = req.body;
   try {
-    const userFromDB = await User.find(user);
+    const userFromDB = await User.findOne(user);
     if (!userFromDB || userFromDB.length === 0) {
       return res.status(404).json({ error: 'User not found' });
     }
